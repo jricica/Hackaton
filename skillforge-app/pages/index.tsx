823ab172ad2d48c762336c7598572bd95d56803d
jricica <janricica@ufm.edu>
2025-06-10 11:54:47 -0700

    index

---
+++ skillforge-app/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to SkillForge!</h1>
+            <p>Your platform to learn and share skills.</p>
+        </div>
+    );
+};
+
+export default Home;

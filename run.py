057082f78e41f11d32222e349387007840141454
jricica <janricica@ufm.edu>
2025-06-10 23:41:42 -0700

    login and register

--- run.py
+++ run.py
@@ -5,6 +5,15 @@ app = Flask(__name__)
 @app.route('/')
 def login():
     return render_template('login.html')
+@app.route('/register')
+
+def register():
+    return render_template('register.html')
+@app.route('/home')
+
+def forgot_password():
+    return render_template('forgot_password.html')
+@app.route('/home')
 
 @app.route('/overview')
 def overview():
@@ -41,6 +50,5 @@ def debate():
 @app.route('/completion')
 def completion():
     return render_template('completion.html')
-
 if __name__ == '__main__':
     app.run(debug=True)

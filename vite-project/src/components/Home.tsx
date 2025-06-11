ffc11e112ed98a8b2f7121199e9d74ee067ec768
jricica <janricica@ufm.edu>
2025-06-10 22:59:24 -0700

    flask

--- vite-project/src/components/Home.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <section>
-      <h2>Bienvenido al programa de escritores liberales</h2>
-      <p>Cada semana los estudiantes exploran un nuevo autor y ponen en práctica sus habilidades de comunicación.</p>
-    </section>
-  );
-}
